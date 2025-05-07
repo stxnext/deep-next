9dd2b401d8fc7338f0d5d31b1a7749a5f78513d9
Patryk Laskowski <patryk.laskowski@stxnext.pl>
2025-05-07 12:31:17 +0200

    improve code review

--- libs/core/deep_next/core/config.py
+++ libs/core/deep_next/core/config.py
@@ -8,7 +8,7 @@ ROOT_DIR = SRC_DIR.parent.parent  # 🛠core
 DATA_DIR = MONOREPO_DATA_PATH / "core"
 DATA_DIR.mkdir(exist_ok=True, parents=True)
 
-AUTOMATED_CODE_REVIEW_MAX_ATTEMPTS = 2
+AUTOMATED_CODE_REVIEW_MAX_ATTEMPTS = 1
 
 
 class SRFConfig:
