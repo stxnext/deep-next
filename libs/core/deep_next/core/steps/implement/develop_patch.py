aea39588f1583a59249264fb2c069baff458a0ee
jedlikowskip <patryk.jedlikowski@stxnext.pl>
2025-05-16 12:57:17 +0200

    prompt_x.py files

--- libs/core/deep_next/core/steps/implement/develop_patch.py
+++ libs/core/deep_next/core/steps/implement/develop_patch.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import difflib
-import textwrap
 from pathlib import Path
 from typing import List
 
@@ -10,254 +9,30 @@ from deep_next.core.parser import has_tag_block, parse_tag_block
 from deep_next.core.steps.action_plan.data_model import Step
 from deep_next.core.steps.implement import acr
 from deep_next.core.steps.implement.common import _create_llm
+from deep_next.core.steps.implement.prompt_all_at_once_implemetation import (
+    PromptAllAtOnceImplementation,
+)
+from deep_next.core.steps.implement.prompt_single_file_implementation import (
+    PromptSingleFileImplementation,
+)
 from deep_next.core.steps.implement.utils import CodePatch
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from loguru import logger
 
 
-class Prompt:
-    task_description = textwrap.dedent(
-        """
-        You are a Senior Python Developer hired to contribute into an existing Python codebase.
-        Your only responsibility is to write source code.
-
-        Ensure that each piece of the code you modify integrates seamlessly with the whole system.
-        Improve modules one by one, with respect to modifications details given by your colleague solution designer.
-
-        DEVELOPMENT GUIDELINES
-        ------------------------
-        - Type Hints: Use type hints to enhance code clarity and maintainability.
-        - Docstrings: Include concise, informative single-line docstrings for all functions and methods.
-        - Line Length: Preferably fit in 88 chars in line.
-        - Pythonic Code: Embrace the Zen of Python by writing simple, readable, and direct code.
-        ------------------------
-
-        BEST PRACTICES
-        ------------------------
-        - Implement Object-Oriented Programming (OOP) principles effectively, particularly those from the SOLID principles.
-        - Write functions that are small, have clear purposes, and bear meaningful names.
-        - Avoid hardcoded values; instead, utilize configuration files or env variables.
-        - Ensure the code is easily testable and extendable, preparing for possible future requirements.
-        ------------------------
-        """  # noqa: E501
-    )
-
-    expected_input_data_description = textwrap.dedent(
-        """
-        EXPECTED INPUT DATA
-        ------------------------
-        1. File To Change
-        The specific file assigned to you that requires modifications.
-
-        2. High-lvl description
-        High-level description of necessary changes in a given file.
-
-        3. Detailed Description
-        Additional requirements needed in the module to meet the expectations.
-
-        4. Issue statement
-        Completes the context for better understanding given requirements.
-        ------------------------
-        """  # noqa: E501
-    )
-
-    input_data = textwrap.dedent(
-        """
-        INPUT DATA
-        ------------------------
-        ```
-        {code_context}
-        ```
-
-        <high_level_description>
-        {high_level_description}
-        </high_level_description>
-
-        <description>
-        {description}
-        </description>
-
-        <issue_statement>
-        {issue_statement}
-        </issue_statement>
-        {git_diff}
-        ------------------------
-        """
-    )
-
-    expected_output_format = textwrap.dedent(
-        """
-        EXPECTED OUTPUT
-        ------------------------
-        Write a patch for the issue, based on the retrieved context.
-
-        Within `<file>...</file>`, replace `...` with actual absolute file path.
-
-        Within `<original>...</original>`, replace `...` with the only and exactly the \
-        same fragment of code without losing any details, mind spaces gaps and \
-        comments. It must be a substring of the original file - don't change any sign.
-
-        The `<original>...</original>` code must be at least 5 lines. It will be used \
-        to identify the exact location of the code to be replaced. The code will be \
-        replaced using the exact match procedure with respect to the lines so DO NOT \
-        change ANY sign.
-
-        Within `<patched>...</patched>` code, pay attention to indentation, it's \
-        Python code.
-
-        {single_modification_output_format}
-
-        If you want to add new code, DO NOT output only empty line in the 'original' \
-        block. Use part of the existing code near which you want to add the new code. \
-        Remember to put the same part of the existing code in the 'patched' field in \
-        order to preserve the original code.
-
-        You can import necessary libraries.
-
-        Note: It's allowed to write multiple modifications for one file.
-        ------------------------
-        """  # noqa: E501
-    )
-
-    # TODO: remove `But don't produce multiple modifications for the same code snippet`
-    #   when handling multiple modifications for the same code snippet is solved
-    modifications_specific_guidelines = textwrap.dedent(
-        """
-        MODIFICATIONS SPECIFIC GUIDELINES
-        ------------------------
-        - Remember that `<original>...</original>` will be replaced with `<patched>...</patched>`.
-        - Multiple small modifications are better than one big modification.
-        - Don't produce multiple modifications for the same code snippet, preventing overlapping changes.
-        - Focus on one function/method at the time.
-        - Don't address the whole file in one modification. Split into multiple logical peaces.
-        - Single modification should consist only of code relevant to itself.
-        - Only produce modifications that introduce a change!
-        - Do not make type hints and doc strings changes if they are not required by the task.
-        - Pay attention to the git diff of the previous modifications made by other developers. They are related to the same task you are part of, and may be worth building upon.
-        ------------------------
-        """  # noqa: E501
-    )
-
-    modifications_example = textwrap.dedent(
-        """
-        MODIFICATIONS EXAMPLE
-        ------------------------
-        ## File to change
-
-        File: /home/patryk/my_project/src/hello_world.py
-        ```python
-        def say_hello():
-            \"\"\"Say hello!\"\"\"
-            print("Hello World")
-
-
-        def log(msg: str) -> None:
-            print(f">>> {{msg}}")
-
-
-        def foo() -> str:
-            return "bar"
-
-
-        def add_integers(a, b):
-            return int(a + b)
-        ```
-
-        {logger_file_example}
-
-        ----------
-        ## Goal
-        {multiple_modification_code_example}
-        Introduce Python 3.11 syntax type hints and implement remove_integers function.
-
-        ## Modifications
-        <modifications>
-        # modification 1
-        <file>/home/patryk/my_project/src/hello_world.py</file>
-        <original>
-        def say_hello():
-            \"\"\"Say hello!\"\"\"
-            print("Hello World")
-
-        def log(msg: str) -> None:
-            print(f">>> {{msg}}")
-        </original>
-        <patched>
-        def say_hello() -> None:
-            \"\"\"Say hello!\"\"\"
-            print("Hello World")
-
-        def log(msg: str) -> None:
-            print(f">>> {{msg}}")
-        </patched>
-
-        # modification 2
-        <file>/home/patryk/my_project/src/hello_world.py</file>
-        <original>
-        def foo() -> str:
-            return "bar"
-
-
-        def add_integers(a, b):
-            return int(a + b)
-        </original>
-        <patched>
-        def foo() -> str:
-            return "bar"
-
-
-        def add_integers(a: int, b: int) -> int:
-            \"\"\"Add two integers.\"\"\"
-            return int(a + b)
-        </patched>
-
-        # modification 3
-        <file>/home/patryk/my_project/src/hello_world.py</file>
-        <original>
-        def foo() -> str:
-            return "bar"
-
-
-        def add_integers(a: int, b: int) -> int:
-            \"\"\"Add two integers.\"\"\"
-            return int(a + b)
-        </original>
-        <patched>
-        def foo() -> str:
-            return "bar"
-
-
-        def add_integers(a: int, b: int) -> int:
-            \"\"\"Add two integers.\"\"\"
-            return int(a + b)
-
-
-        def remove_integers(a: int, b: int) -> int:
-            \"\"\"Subtract two integers.\"\"\"
-            return int(a - b)
-        </patched>
-        </modifications>
-        ------------------------
-
-        Notice that in # modification 2 type hints and docstrings were added to the \
-        `add_integers` function and this is reflected in the # modification 3.
-        If your task is to implement new function then attach context of the file \
-        where this function should be implemented, i.e. `remove_integers` function.
-        """
-    )
-
-
-def _create_llm_agent():
+def _create_llm_agent(
+    prompt: PromptSingleFileImplementation | PromptAllAtOnceImplementation,
+):
     """Creates LLM agent for project description task."""
     develop_changes_prompt_template = ChatPromptTemplate.from_messages(
         [
-            ("system", Prompt.task_description),
-            ("human", Prompt.expected_input_data_description),
-            ("human", Prompt.input_data),
-            ("human", Prompt.expected_output_format),
-            ("human", Prompt.modifications_specific_guidelines),
-            ("human", Prompt.modifications_example),
+            ("system", prompt.task_description),
+            ("human", prompt.expected_input_data_description),
+            ("human", prompt.input_data),
+            ("human", prompt.expected_output_format),
+            ("human", prompt.modifications_specific_guidelines),
+            ("human", prompt.modifications_example),
         ]
     )
 
@@ -277,27 +52,14 @@ def develop_single_file_patches(step: Step, issue_statement: str, git_diff: str)
         with open(step.target_file, "w") as f:
             f.write("# Comment added at creation time to indicate empty file.\n")
 
-    single_modification_output_format = textwrap.dedent(
-        """
-    The requested modifications you are assigned with are part of a larger coding \
-    task. For context, you are given the task issue statement. Some code changes \
-    have already been made for the task by other developers. When developing the \
-    your modifications, build upon the changes made so far (if plausible).
-    """
-    )
-
-    raw_edits = _create_llm_agent().invoke(
+    raw_edits = _create_llm_agent(PromptSingleFileImplementation).invoke(
         {
-            "code_context": (
-                f"File path: '{step.target_file}'\n{read_txt(step.target_file)}"
-            ),
+            "path": step.target_file,
+            "code_context": read_txt(step.target_file),
             "high_level_description": step.title,
             "description": step.description,
             "issue_statement": issue_statement,
-            "single_modification_output_format": single_modification_output_format,
-            "logger_file_example": "",
-            "multiple_modification_code_example": "",
-            "git_diff": f" <git_diff>\n{git_diff}\n</git_diff>",
+            "git_diff": git_diff,
         }
     )
 
@@ -338,63 +100,12 @@ def develop_all_patches(steps: List[Step], issue_statement: str) -> str:
             f"```python\n{read_txt(step.target_file)}\n```\n"
         )
 
-    logger_file_example = textwrap.dedent(
-        """
-        File: /home/patryk/my_project/src/logger.py
-        ```python
-        # Placeholder for code content
-        ```
-
-        """
-    )
-
-    multiple_modification_code_example = textwrap.dedent(
-        """
-        Implement logger and add logging functionality to the `say_hello` function in `hello_world.py`.
-
-        ## Modifications
-        <modifications>
-        # modification 1
-        <file>/home/patryk/my_project/src/logger.py</file>
-        <original>
-        # Placeholder for code content
-        </original>
-        <patched>
-        from loguru import logger
-        logger.add("file.log", rotation="1 MB")
-
-        def log(msg: str) -> None:
-            logger.info(f">>> {{msg}}")
-        </patched>
-        # modification 2
-        <file>/home/patryk/my_project/src/hello_world.py</file>
-        <original>
-        def say_hello():
-            \"\"\"Say hello!\"\"\"
-            print("Hello World")
-        </original>
-        <patched>
-        def say_hello():
-            \"\"\"Say hello!\"\"\"
-            print("Hello World")
-            log("Hello World printed")
-        </patched>
-
-        ----------
-        ## Goal (Another example)
-        """  # noqa: E501
-    )
-
-    raw_modifications = _create_llm_agent().invoke(
+    raw_modifications = _create_llm_agent(PromptAllAtOnceImplementation).invoke(
         {
             "issue_statement": issue_statement,
             "description": steps_description,
             "code_context": files_content,
             "high_level_description": step.title,
-            "single_modification_output_format": "",
-            "logger_file_example": logger_file_example,
-            "multiple_modification_code_example": multiple_modification_code_example,
-            "git_diff": "",
         }
     )
 
