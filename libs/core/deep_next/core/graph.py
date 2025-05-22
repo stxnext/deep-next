3e5106ad558c4175fa2718eaf43c93c6468b7458
pjedlikowskistx <patryk.jedlikowski@stxnext.pl>
2025-05-22 14:10:40 +0200

    Update libs/core/deep_next/core/graph.py
    Co-authored-by: Patryk Laskowski <patryk.laskowski@stxnext.pl>

--- libs/core/deep_next/core/graph.py
+++ libs/core/deep_next/core/graph.py
@@ -160,7 +160,6 @@ class DeepNextGraph(BaseGraph):
         self.add_quick_edge(_Node.gather_project_knowledge, _Node.create_action_plan)
         self.add_quick_edge(_Node.create_action_plan, _Node.implement)
 
-        # if we want to run SWE evaluation then it should be comment out
         self.add_quick_edge(_Node.implement, _Node.review_code)
         self.add_quick_edge(
             _Node.prepare_automated_code_review_changes, _Node.gather_project_knowledge
